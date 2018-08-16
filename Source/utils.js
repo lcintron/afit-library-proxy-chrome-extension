c6e85e067afa00c569b7583deddd5dff517989fb
Luis Cintron <l.cintron@Live.com>
2018-08-15 20:28:12 -0400

    Renamed tests for utils

--- Source/utils.js
+++ Source/utils.js
@@ -1,29 +1,29 @@
-function Utils(){};
+function Utils() { };
 
-Utils.compareUrlDomain = function(sourceUrl, urlDomainToMatch) {
-    if (sourceUrl.length >= urlDomainToMatch.length) {
-        let j = 0;
-        for (let i = 0; i < urlDomainToMatch.length; i++) {
-            while(urlDomainToMatch[i] === '*' && j < sourceUrl.length && sourceUrl[j]!=='.'){
-                j++;
-                if(j>= sourceUrl.length)
-                    return true;
-            }
+Utils.compareUrlDomain = function (sourceUrl, urlDomainToMatch) {
+    if (sourceUrl.length < urlDomainToMatch.length)
+        return false;
 
-            if(urlDomainToMatch[i] === '*' && sourceUrl[j] ==='.'){
-                i++;
-            }
-            else if(urlDomainToMatch[i] !== sourceUrl[j]){
-                return false;
-            }
-
+    let j = 0;
+    for (let i = 0; i < urlDomainToMatch.length; i++) {
+        while (urlDomainToMatch[i] === '*' && j < sourceUrl.length && sourceUrl[j] !== '.') {
             j++;
+            if (j >= sourceUrl.length)
+                return true;
         }
-    }
 
+        if (urlDomainToMatch[i] === '*' && sourceUrl[j] === '.') {
+            i++;
+        }
+        else if (urlDomainToMatch[i] !== sourceUrl[j]) {
+            return false;
+        }
+
+        j++;
+    }
     return true;
 }
-Utils.prettyDate = function (now, time){
+Utils.prettyDate = function (now, time) {
     var date = new Date(time || ""),
         diff = (((new Date(now)).getTime() - date.getTime()) / 1000),
         day_diff = Math.floor(diff / 86400);
@@ -33,12 +33,12 @@ Utils.prettyDate = function (now, time){
     }
 
     return day_diff == 0 && (
-            diff < 60 && "just now" ||
-            diff < 120 && "1 minute ago" ||
-            diff < 3600 && Math.floor( diff / 60 ) + " minutes ago" ||
-            diff < 7200 && "1 hour ago" ||
-            diff < 86400 && Math.floor( diff / 3600 ) + " hours ago") ||
+        diff < 60 && "just now" ||
+        diff < 120 && "1 minute ago" ||
+        diff < 3600 && Math.floor(diff / 60) + " minutes ago" ||
+        diff < 7200 && "1 hour ago" ||
+        diff < 86400 && Math.floor(diff / 3600) + " hours ago") ||
         day_diff == 1 && "Yesterday" ||
         day_diff < 7 && day_diff + " days ago" ||
-        day_diff < 31 && Math.ceil( day_diff / 7 ) + " weeks ago";
+        day_diff < 31 && Math.ceil(day_diff / 7) + " weeks ago";
 }
--- Test/tests.js
+++ Test/tests.js
@@ -1,6 +1,7 @@
-QUnit.test( "Utils Tests", function( assert ) {
+QUnit.test( "Utils.compareUrlDomain Test", function( assert ) {
     assert.ok(Utils.compareUrlDomain('https://www.scitation.org', 'https://www.scitation.org'), "Expect True with Exact Same Url" );
     assert.ok(!Utils.compareUrlDomain('https://www.scitation.org', 'http://www.scitation.org'), "Expect False with Different Protocol Url" );
+    assert.ok(!Utils.compareUrlDomain('https://www.facebook.com', 'https://ieeexplore.ieee.org'), "Expect False with Different Domain Url" );
     assert.ok(Utils.compareUrlDomain('https://www.scitation.org/this/is/a/test', 'https://www.scitation.org'), "Expect True same Url different paths" );
     assert.ok(Utils.compareUrlDomain('https://www.scitation.org', 'https://*.scitation.org'), "Expect True same urls, wildcard(*) for subdomain - no path" );
     assert.ok(Utils.compareUrlDomain('https://somethingelsethatwedontknow.scitation.org/this/is/a/test', 'https://*.scitation.org'), "Expect True same TLD urls, wildcard(*) for subdomain" );
@@ -8,5 +9,4 @@ QUnit.test( "Utils Tests", function( assert ) {
     assert.ok(!Utils.compareUrlDomain('https://api.scitation1.org/this/is/a/test', 'https://*.scitation.org'), "Expect False diff domain urls, wildcard(*) for subdomain - diff path" );
     assert.ok(!Utils.compareUrlDomain('https://api.scitation.1org/this/is/a/test', 'https://*.scitation.org'), "Expect False diff TLD urls, wildcard(*) for subdomain - diff path" );
     assert.ok(!Utils.compareUrlDomain('https://aip-scitation-org.afit.idm.oclc.org/action/doSearch?appendWebsiteFilter=false&AllField=software', 'https://*.scitation.org'), "Expect False diff diff urls, wildcard(*) for subdomain - diff path" );
-    
 });
