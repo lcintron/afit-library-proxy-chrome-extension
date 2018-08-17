1a82fdddf32575aca75bb78eef41fe1b13b35fa5
Luis Cintron <l.cintron@Live.com>
2018-08-17 12:53:58 -0400

    Updated background js to use other url for login

--- Source/background.js
+++ Source/background.js
@@ -4,11 +4,11 @@
 //Runs when the extension is loaded for the first time.
 const dbs = [
 	'https://ieeexplore.ieee.org'
-	,'https://dl.acm.org'
-	,'https://ascelibrary.org'
-	,'https://search.proquest.com'
-	,'https://www.sciencedirect.com'
-	,'https://*.scitation.org'
+	, 'https://dl.acm.org'
+	, 'https://ascelibrary.org'
+	, 'https://search.proquest.com'
+	, 'https://www.sciencedirect.com'
+	, 'https://*.scitation.org'
 ];
 
 document.addEventListener('DOMContentLoaded', function () {
@@ -20,12 +20,13 @@ chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
 	if (changeInfo.status === 'loading' || changeInfo.status === 'complete') {
 		chrome.tabs.query({ 'active': true, 'lastFocusedWindow': true }, function (tabs) {
 
-			if(dbs.find(url => Utils.compareUrlDomain(tab.url, url))){
+			if (dbs.find(url => Utils.compareUrlDomain(tab.url, url))) {
 				let rUrl = 'https://afit.idm.oclc.org/login?url=' + tab.url;
 				chrome.tabs.update(tab.id, { url: rUrl });
 			}
 			//if afit login site - inject js to post login
-			else if (changeInfo.status === 'complete' && tab.url.startsWith('https://afit.idm.oclc.org/login')) {
+			else if (changeInfo.status === 'complete' 
+			&& (tab.url.startsWith('https://afit.idm.oclc.org/login') || tab.url.startsWith('https://login.afit.idm.oclc.org/login'))) {
 				chrome.storage.sync.get(['userInfo'], function (items) {
 					if (items.userInfo && items.userInfo.user && items.userInfo.pass && items.userInfo.verified)
 						chrome.tabs.executeScript(tabId, { code: 'var afitlibuser="' + items.userInfo.user + '";var afitlibpass="' + items.userInfo.pass + '";' }, function () {
